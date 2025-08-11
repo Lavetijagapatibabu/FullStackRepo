b7cce4be4b17530f3d03b0dcce84a01a8bf2c4bd
jagadeesh laveti <jagadeesh laveti@jagapatibabu>
2025-08-11 23:09:28 +0530

    commit and push

---
+++ GitProj01_Addition/src/main/java/com/jb/main/Addition.java
@@ -0,0 +1,14 @@
+package com.jb.main;
+
+public class Addition {
+
+	public Addition() {
+		System.out.println("Addition.0-Param()");
+	}
+	public int performingAddtion() {
+		int a=10;
+		int b=20;
+		return a+b;
+	}
+
+}
