1c1589d45736982bfbb947ac9fef78da58b8abb0
jagadeesh laveti <jagadeesh laveti@jagapatibabu>
2025-08-12 13:11:33 +0530

    commit and push

--- GitProj01_Addition/src/main/java/com/jb/payments/UPIPayments.java
+++ GitProj01_Addition/src/main/java/com/jb/payments/UPIPayments.java
@@ -5,12 +5,15 @@ public class UPIPayments {
 	public String phonePe(String item,int amount) {
 		return item+" Cost is ::"+amount;
 	}
+
+
 	public String paytm (String item,int amount) {
 		return item+" Cost is ::"+amount;
 	}
-	public String Gpay(String item,int amount) {
+
+
+	public String PayZapp(String item,int amount) {
 		return item+" Cost is ::"+amount;
 	}
-
 
 }
