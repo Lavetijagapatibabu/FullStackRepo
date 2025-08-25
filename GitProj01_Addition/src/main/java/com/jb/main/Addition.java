30cc9a557f8c16b94213c0b3088f3c406865c8a4
jagadeesh laveti <jagadeesh laveti@jagapatibabu>
2025-08-25 13:10:09 +0530

    commit and push

--- GitProj01_Addition/src/main/java/com/jb/main/Addition.java
+++ GitProj01_Addition/src/main/java/com/jb/main/Addition.java
@@ -2,34 +2,5 @@ package com.jb.main;
 
 public class Addition {
 
-	public Addition() {
-		System.out.println("Addition.0-Param()");
-	}
-	public int performingAddtion() {
-		int a=10;
-		int b=20;
-		//performing Addition Operation
-		int z=a+b;
-		return z;
-	}
-	public int performingSubStraction() {
-		int a=20;
-		int b=10;
-		return a-b;
-	}
-
-	public int performingMultiplication() {
-		int a =10;
-		int b=20;
-		return a*b;
-	}
-	public float performingDivision() {
-		int a=10;
-		int b=20;
-		return a/b;
-	}
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
 
 }
